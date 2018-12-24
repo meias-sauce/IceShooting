c99222bc8b742a189cc1044de5afc96e4fa6c222
Kazuki Otani <35895778+meias-sauce@users.noreply.github.com>
2018-12-25 03:09:39 +0900

    mainの移動、基本事項書き込み
    めっちゃコメント書いた

---
+++ source/cpp/main.cpp
@@ -0,0 +1,50 @@
+#include "../header/main.h"
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//ログファイルを出さない設定
+	SetOutApplicationLogValidFlag(FALSE);
+
+	//ウィンドウモードで起動
+	ChangeWindowMode(TRUE);
+
+	//ウィンドウサイズ設定
+	SetGraphMode(640, 480, 32);
+	SetWindowSize(640, 480);
+
+	//バックグラウンドでも動作する設定
+	SetAlwaysRunFlag(true);
+
+	//アーカイブファイルのパス設定、コード公開してるので実はガバガバ
+	SetDXArchiveKeyString("atuikoori");
+	//アーカイブファイルの拡張子設定
+	SetDXArchiveExtension("sgp");
+
+	//アイコンファイルの読み込み
+	//SetWindowIconID(IDI_ICON2);
+
+	//ウィンドウタイトルの設定
+	SetMainWindowText("最高にアツいシューティングを頼む");
+
+
+	//DxLib初期化
+	if (DxLib_Init() == -1)	{
+		return -1;
+	}
+
+	//描画先を裏画面に設定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//メインループ
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0 /*&& gpUpdateKey() == 0*/) {
+		//printfDxを消去
+		clsDx();
+
+		printfDx("動作正常");
+	}
+
+	DxLib_End();
+
+	return 0;
+}
